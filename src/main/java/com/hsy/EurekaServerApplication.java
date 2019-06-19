e8288bf4b45133935b07200a7768466493e17334
jian.zhangg <34733800+jianzhangg@users.noreply.github.com>
2019-06-19 16:56:21 +0800

    Add files via upload
    init

---
+++ src/main/java/com/hsy/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.hsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 张梓枫
+ * @date:  2019年3月14日 下午4:32:13  
+ * @Description:  注册中心
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
